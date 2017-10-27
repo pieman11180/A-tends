514ff0187402a455e75dd4e90791aec70a501e51
colli3sm <sean.mc4@gmail.com>
2017-10-27 12:50:09 -0400

    Methods for screens
    Attached are the methods for each screen (read the psuedocode from Phase III report). These can be copy and pasted onto their respective screens. Because they are all in one class file,  some common variables and methods will throw re-initialization errors. These will go away when they are pasted into their respective screens.

---
+++ Workflow.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A__endance
+{
+    class Workflow
+    {
+        //Opening Screen
+        String ID;
+        String barcode;
+        User user;
+        String readBarCode() { return null; }
+        String parseBarCode() { return null; }
+        User searchDataBase() { return null; }
+        void login() { }
+
+        //Professor Screen
+        Professor prof;
+        ArrayList classList;
+        void viewCourses() { }
+        void startAttendance() { }
+
+        //ClockIn Screen
+        CourseDate CD; //today's coursedate
+        ArrayList CDSList;//coursedatestudent list
+        void clockIn(String barcode) { }
+        void clockOut(String barcode) { }
+        void save() { } //save cdslist results to database
+        void exit() { } //will ask if save
+
+        //ViewCourses Screen
+        ArrayList CList;//courses
+        Course selectCourse() { return null; }
+        void back() { }
+
+        //Course SCreen
+        ArrayList CDList;//list of CourseDates
+        ArrayList CSList;//list of CourseStudents
+        void loadCDList() { }
+        void loadCSList() { }
+        void selectCourseDate() { }
+        void selectCourseStudent() { }
+
+        //CourseDateScreen
+        ArrayList CDSList; //list of CourseDateStudents
+        void loadCDSList() { }
+        void editCDS() { }
+
+        //CourseStudent Screen
+        //list of CourseDateStudents matching this students
+        ArrayList CDSList;
+        void loadCDSList() { }
+        void editCDS() { }
+
+
+
+    }
+}
